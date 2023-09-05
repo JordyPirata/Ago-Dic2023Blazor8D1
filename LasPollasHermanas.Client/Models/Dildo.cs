f74fb5b6bd1e78e308037258ba40819f136b69d4
JordyPirata <jordi77alva@gmail.com>
2023-09-04 22:40:13 -0600

    Ad Required

--- LasPollasHermanas.Client/Models/Dildo.cs
+++ LasPollasHermanas.Client/Models/Dildo.cs
@@ -14,7 +14,9 @@ namespace LasPollasHermanas.Client.Models
         public string? Name { get; set; }
         [Required][Range (1,100)]
         public decimal Price { get; set; }
+        [Required]
         public decimal Size { get; set; }
+        [Required]
         public DateTime ExpireDate { get; set; }
         public string? Material { get; set; }
         public string? Color { get; set; }
